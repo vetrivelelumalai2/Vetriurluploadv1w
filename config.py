0535bcc39f4dcf2c7fc37f9dce8a4f82dc4c3163
knox <111348305+aceknox@users.noreply.github.com>
2023-01-26 12:51:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5932217090:AAFBletkW0lWlva8EdU1ALbDMKcTd4NbJLs")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", "28457125"))
-    API_HASH = os.environ.get("API_HASH","c25bda5d2624db06db8a1624d4e484ce")
+    API_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH","")
     # Get these values from my.telegram.org
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -26,10 +26,10 @@ class Config(object):
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", "654804764"))
+    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
     SESSION_NAME = "URLUPLOADERBOT"
     #log channel
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1001823745911"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Knoxop24:knoxop24@cluster0.f6m6fwo.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "")
     MAX_RESULTS = "50"
