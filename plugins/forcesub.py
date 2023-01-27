b2f0a4108a92f190cd8cc0f5c6e3222424e56b89
knox <111348305+aceknox@users.noreply.github.com>
2023-01-27 09:03:28 +0530

    Update forcesub.py

--- plugins/forcesub.py
+++ plugins/forcesub.py
@@ -1,5 +1,50 @@
-# soon...
+# enable if you want it 
 """
-from config import Config
-from pyrogram.errors import UserNotParticipant
+import asyncio
+from plugins.config import Config
+from pyrogram import Client
+from pyrogram.errors import FloodWait, UserNotParticipant
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+async def handle_force_subscribe(bot, message):
+    try:
+        invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return 400
+    try:
+        user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), message.from_user.id)
+        if user.status == "kicked":
+            await bot.send_message(
+                chat_id=message.from_user.id,
+                text="Sorry , You are Banned. Contact My [Support Group](https://t.me/knoxbots).",
+                parse_mode="markdown",
+                disable_web_page_preview=True,
+                reply_to_message_id=message.message_id,
+            )
+            return 400
+    except UserNotParticipant:
+        await bot.send_message(
+            chat_id=message.from_user.id,
+            text="**Please Join My Update Channel To Use Me**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Join My Update Group ", url=invite_link.invite_link)
+                    ]
+                ]
+            ),
+            parse_mode="markdown",
+            reply_to_message_id=message.message_id,
+        )
+        return 400
+    except Exception:
+        await bot.send_message(
+            chat_id=message.from_user.id,
+            text="Something Went Wrong. Contact My [Support Group](https://t.me/knoxbots).",
+            parse_mode="markdown",
+            disable_web_page_preview=True,
+            reply_to_message_id=message.message_id,
+        )
+        return 400
 """
