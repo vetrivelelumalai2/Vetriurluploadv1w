39da6aa8ab5002abb71bb483290ef5c7e1df4998
knox <111348305+aceknox@users.noreply.github.com>
2023-02-03 10:47:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/aceknox/UrlUploaderBot 
-cd /UrlUploaderBot
+git clone https://github.com/aceknox/Url-Uploader-Bot-V4 
+cd /Url-Uploader-Bot-V4 
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait"
 python3 bot.py
