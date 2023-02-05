686bebfb5c90f7deb0d5460f743d3f143c9d7c25
knox <111348305+aceknox@users.noreply.github.com>
2023-02-05 08:41:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(object):
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 2097152000
+    TG_MAX_FILE_SIZE = 4097152000
     FREE_USER_MAX_FILE_SIZE = 50000000
     VIDEO_FORMATS = ["mp4", "mkv", "mov", "m4v", "avi", "unknown_video"]
     AUDIO_FORMATS = ["mp3", "flac", "m4a", "wav"]
