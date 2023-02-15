9806053a04df8c859c316235f7bdcff3d2666031
vetrivelelumalai2 <68172178+vetrivelelumalai2@users.noreply.github.com>
2023-02-15 22:44:55 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo..."
-git clone https://github.com/aceknox/Url-Uploader-Bot-V4 
-cd /Url-Uploader-Bot-V4 
+git clone https://github.com/vetrivelelumalai2/Vetriurluploadv1w
+cd /Vetriurluploadv1w
 pip3 install -U -r requirements.txt
 echo "Starting Bot.."
 python3 bot.py
